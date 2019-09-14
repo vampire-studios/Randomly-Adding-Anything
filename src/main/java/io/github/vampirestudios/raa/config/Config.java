b32efd435479b0b6bb3fb270f108ec6205809146
Cat Core <arthur.koalas@free.fr>
2019-09-14 14:54:45 +0200

    change Default Material Number

--- src/main/java/io/github/vampirestudios/raa/config/Config.java
+++ src/main/java/io/github/vampirestudios/raa/config/Config.java
@@ -7,7 +7,7 @@ import me.sargunvohra.mcmods.autoconfig1.ConfigData;
 @me.sargunvohra.mcmods.autoconfig1.annotation.Config(name = RandomlyAddingAnything.MOD_ID)
 public class Config implements ConfigData {
 
-    public int materialNumber = 1000;
+    public int materialNumber = 100;
     public boolean debug = true;
     public boolean regen = false;
     public LangEnum namingLanguage = LangEnum.ENGLISH;
