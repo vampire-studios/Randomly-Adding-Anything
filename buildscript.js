e7186ac5fbae595c10f643d2e9a54cb70b0831c0
Cat Core <arthur.koalas@free.fr>
2019-09-14 15:22:54 +0200

    add buildscript

---
+++ buildscript.js
@@ -0,0 +1,22 @@
+const fs = require("fs")
+const consts = {
+    travis_pull_request: process.env.TRAVIS_PULL_REQUEST,
+    travis_branch: process.env.TRAVIS_BRANCH
+}
+
+fs.writeFile("./buildscriptcmd.bash", texttowrite(), (err) => {
+    if (err) {
+        console.log(err)
+    }
+})
+function texttowrite() {
+    if (consts.travis_pull_request === true) {
+        return "./gradlew build"
+    } else {
+        if (consts.travis_branch === "release/1.14") {
+            return "./gradlew build githubRelease curseforge"
+        } else {
+            return "./gradlew build githubRelease"
+        }
+    }
+}
