9bc43a808731a79388895de5f8c1a80d109991d3
Cat Core <arthur.koalas@free.fr>
2019-11-05 22:39:37 +0100

    Some progress on item pickup detection...

--- src/main/java/io/github/vampirestudios/raa/mixins/MixinLivingEntity.java
+++ src/main/java/io/github/vampirestudios/raa/mixins/MixinLivingEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,8 +17,8 @@ public class MixinLivingEntity {
 
     @Inject(method = "sendPickup", at = @At(value = "HEAD"))
     private void onPlayerPickup(Entity entity_1, int int_1, CallbackInfo ci) {
-        if (!entity_1.removed && !((LivingEntity)(Object)this).world.isClient && (entity_1 instanceof ItemEntity || entity_1 instanceof ProjectileEntity || entity_1 instanceof ExperienceOrbEntity)) {
-
+        if (!entity_1.removed && !((LivingEntity)(Object)this).world.isClient && entity_1 instanceof ItemEntity) {
+            System.out.println((((ItemEntity) entity_1).getStack()) + " " + int_1);
         }
     }
 }
