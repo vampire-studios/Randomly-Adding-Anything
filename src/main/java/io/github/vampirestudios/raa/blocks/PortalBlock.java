efa8bef103f88cb006181c12a16c22896db9a78b
Cat Core <arthur.koalas@free.fr>
2019-11-08 16:54:08 +0100

    Revert "Revert "update to 19w45a""
    This reverts commit 8d0fda5c34ee103647ff453d2b15991ce620ab8e.

--- src/main/java/io/github/vampirestudios/raa/blocks/PortalBlock.java
+++ src/main/java/io/github/vampirestudios/raa/blocks/PortalBlock.java
@@ -34,7 +34,7 @@ public class PortalBlock extends Block {
     public ActionResult onUse(BlockState blockState_1, World world_1, BlockPos blockPos_1, PlayerEntity playerEntity_1, Hand hand_1, BlockHitResult blockHitResult_1) {
         if (!world_1.isClient) {
             System.out.println("Space1");
-            if ((int) playerEntity_1.getPos().getX() == blockPos_1.up().method_23228().getX() && (int) playerEntity_1.getPos().getZ() == blockPos_1.up().method_23228().getZ() && (int) playerEntity_1.getPos().getY() == blockPos_1.up().method_23228().getY() + 1) {
+            if ((int) playerEntity_1.getPos().getX() == blockPos_1.up().down().getX() && (int) playerEntity_1.getPos().getZ() == blockPos_1.up().down().getZ() && (int) playerEntity_1.getPos().getY() == blockPos_1.up().down().getY() + 1) {
                 System.out.println("Space");
                 if (playerEntity_1.dimension != this.dimensionType) {
                     playerEntity_1.changeDimension(this.dimensionType);
