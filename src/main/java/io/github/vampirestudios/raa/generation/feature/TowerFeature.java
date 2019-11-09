be5710ade8fcdd70c41b622162350fe1c2ffebe5
SuperCoder7979 <k.pranav@gmail.com>
2019-11-09 03:28:46 -0500

    add new tower features

--- src/main/java/io/github/vampirestudios/raa/generation/feature/TowerFeature.java
+++ src/main/java/io/github/vampirestudios/raa/generation/feature/TowerFeature.java
@@ -23,52 +23,81 @@ public class TowerFeature extends Feature<DefaultFeatureConfig> {
         super(function);
     }
 
+    //Generates offensive/storage towers
+
     @Override
     public boolean generate(IWorld world, ChunkGenerator chunkGenerator, Random random, BlockPos pos, DefaultFeatureConfig config) {
         if (world.getBlockState(pos.add(0, -1, 0)).isAir() || !world.getBlockState(pos.add(0, -1, 0)).isOpaque())
             return true;
+        int tier = 0;
+        if (Rands.chance(10)) tier = 1;
         int height = Rands.randIntRange(6, 15);
         for (int i = 0; i < height; i++) {
             for (int j = -2; j <= 2; j++) {
                 for (int k = -2; k <= 2; k++) {
                     if (j == -2 || j == 2 || k == -2 || k == 2) {
-                        placeBlockAt(world, pos.add(j, i, k));
+                        placeBlockAt(world, pos.add(j, i, k), tier);
                     }
                 }
             }
         }
-        for (int i = height - 2; i < height + Rands.randIntRange(3, 5); i++) {
+        int heightTotal = height + Rands.randIntRange(3, 5);
+        for (int i = height - 2; i < heightTotal; i++) {
             for (int j = -3; j <= 3; j++) {
                 for (int k = -3; k <= 3; k++) {
                     if (j == -3 || j == 3 || k == -3 || k == 3) {
-                        placeBlockAt(world, pos.add(j, i, k));
+                        placeBlockAt(world, pos.add(j, i, k), tier);
                     }
                     if (j == -2 && k == -2 && i == height) {
                         world.setBlockState(pos.add(j, i, k), StructurePiece.method_14916(world, pos, Blocks.CHEST.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.SOUTH)), 2);
-                        LootableContainerBlockEntity.setLootTable(world, Rands.rand, pos.add(j, i, k), (Rands.chance(5)) ? LootTables.SIMPLE_DUNGEON_CHEST : RAALootTables.TOWER_LOOT);
+                        LootableContainerBlockEntity.setLootTable(world, Rands.rand, pos.add(j, i, k), (tier == 1) ? LootTables.SIMPLE_DUNGEON_CHEST : RAALootTables.TOWER_LOOT);
                     }
+                    if (tier == 1) {
+                        if (j == 2 && k == 2 && i == height) {
+                            world.setBlockState(pos.add(j, i, k), StructurePiece.method_14916(world, pos, Blocks.CHEST.getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH)), 2);
+                            LootableContainerBlockEntity.setLootTable(world, Rands.rand, pos.add(j, i, k), LootTables.SIMPLE_DUNGEON_CHEST);
+                        }
+                    }
+                }
+            }
+        }
+        if (Rands.chance(2) || tier == 1) {
+            for (int j = -3; j <= 3; j++) {
+                for (int k = -3; k <= 3; k++) {
+                    placeBlockAt(world, pos.add(j, heightTotal-1, k), tier);
                 }
             }
         }
         return true;
     }
 
-    public static void placeBlockAt(IWorld world, BlockPos pos) {
-        int rand = Rands.randInt(5);
-        switch (rand) {
-            case 0:
-            case 4:
-                world.setBlockState(pos, Blocks.STONE_BRICKS.getDefaultState(), 2);
-                break;
-            case 1:
-                world.setBlockState(pos, Blocks.CRACKED_STONE_BRICKS.getDefaultState(), 2);
-                break;
-            case 2:
-                world.setBlockState(pos, Blocks.MOSSY_STONE_BRICKS.getDefaultState(), 2);
-                break;
-            case 3:
-                world.setBlockState(pos, Blocks.AIR.getDefaultState(), 2);
-                break;
+    public static void placeBlockAt(IWorld world, BlockPos pos, int tier) {
+        if (tier == 0) {
+            int rand = Rands.randInt(5);
+            switch (rand) {
+                case 0:
+                case 4:
+                    world.setBlockState(pos, Blocks.STONE_BRICKS.getDefaultState(), 2);
+                    break;
+                case 1:
+                    world.setBlockState(pos, Blocks.CRACKED_STONE_BRICKS.getDefaultState(), 2);
+                    break;
+                case 2:
+                    world.setBlockState(pos, Blocks.MOSSY_STONE_BRICKS.getDefaultState(), 2);
+                    break;
+                case 3:
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState(), 2);
+                    break;
+            }
+        } else {
+            int rand = Rands.randInt(8);
+            switch (rand) {
+                case 4:
+                    world.setBlockState(pos, Blocks.AIR.getDefaultState(), 2);
+                    break;
+                default:
+                    world.setBlockState(pos, Blocks.BRICKS.getDefaultState(), 2);
+            }
         }
     }
 }
