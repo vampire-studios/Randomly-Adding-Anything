c54682b60d619a9a9008031e6595a3ffc5d1a110
shedaniel <daniel@shedaniel.me>
2019-11-16 19:50:08 +0800

    fix crash

--- src/main/java/io/github/vampirestudios/raa/utils/GsonUtils.java
+++ src/main/java/io/github/vampirestudios/raa/utils/GsonUtils.java
@@ -7,6 +7,8 @@ import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonToken;
 import com.google.gson.stream.JsonWriter;
+import io.github.vampirestudios.raa.RandomlyAddingAnything;
+import io.github.vampirestudios.raa.api.enums.GeneratesIn;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
 
@@ -25,30 +27,55 @@ public class GsonUtils {
 		return new Identifier(JsonHelper.getString(jsonObject, "field_13353"), JsonHelper.getString(jsonObject, "field_13355"));
 	}
 
-	static {
-		GSON = new GsonBuilder()
-			.registerTypeAdapter(Identifier.class, new TypeAdapter<Identifier>() {
-				@Override
-				public void write(JsonWriter out, Identifier value) throws IOException {
-					if(value == null)
-						out.nullValue();
-					else
-						out.value(value.toString());
-				}
+    static {
+        GSON = new GsonBuilder()
+                .registerTypeAdapter(Identifier.class, new TypeAdapter<Identifier>() {
+                    @Override
+                    public void write(JsonWriter out, Identifier value) throws IOException {
+                        if (value == null)
+                            out.nullValue();
+                        else
+                            out.value(value.toString());
+                    }
 
-				@Override
-				public Identifier read(JsonReader in) throws IOException {
-					JsonToken jsonToken = in.peek();
-					if(jsonToken == JsonToken.NULL) {
-						in.nextNull();
-						return null;
-					} else {
-						return new Identifier(in.nextString());
-					}
-				}
-			})
-			.serializeNulls()
-			.setPrettyPrinting()
-			.create();
-	}
+                    @Override
+                    public Identifier read(JsonReader in) throws IOException {
+                        JsonToken jsonToken = in.peek();
+                        if (jsonToken == JsonToken.NULL) {
+                            in.nextNull();
+                            return null;
+                        } else {
+                            return new Identifier(in.nextString());
+                        }
+                    }
+                })
+                .registerTypeAdapter(GeneratesIn.class, new TypeAdapter<GeneratesIn>() {
+                    @Override
+                    public void write(JsonWriter out, GeneratesIn value) throws IOException {
+                        if (value == null)
+                            out.nullValue();
+                        else
+                            out.value(value.getIdentifier().toString());
+                    }
+
+                    @Override
+                    public GeneratesIn read(JsonReader in) throws IOException {
+                        JsonToken jsonToken = in.peek();
+                        if (jsonToken == JsonToken.NULL) {
+                            in.nextNull();
+                            return null;
+                        } else {
+                            String s = in.nextString();
+                            Identifier identifier = s.contains(":") ? new Identifier(s.toLowerCase()) : new Identifier(RandomlyAddingAnything.MOD_ID, s.toLowerCase());
+                            for (GeneratesIn value : GeneratesIn.getValues())
+                                if (value.getIdentifier().equals(identifier))
+                                    return value;
+                            throw new NullPointerException("Invalid GeneratesIn: " + identifier.toString());
+                        }
+                    }
+                })
+                .serializeNulls()
+                .setPrettyPrinting()
+                .create();
+    }
 }
