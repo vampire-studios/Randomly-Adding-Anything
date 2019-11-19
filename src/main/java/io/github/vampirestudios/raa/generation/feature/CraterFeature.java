2a8ce531ce6d0370de4a4a035c36ee1d3c993cbd
HunsingerTheMatist <madhunsinger@gmail.com>
2019-11-19 02:31:13 -0500

    Added in Towers and /locateRAA command for structures

--- src/main/java/io/github/vampirestudios/raa/generation/feature/CraterFeature.java
+++ src/main/java/io/github/vampirestudios/raa/generation/feature/CraterFeature.java
@@ -1,5 +1,4 @@
 package io.github.vampirestudios.raa.generation.feature;
-
 import com.mojang.datafixers.Dynamic;
 import io.github.vampirestudios.raa.generation.feature.config.CorruptedFeatureConfig;
 import io.github.vampirestudios.raa.utils.Rands;
@@ -8,6 +7,7 @@ import net.minecraft.block.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
 
 import java.util.ArrayList;
@@ -15,22 +15,14 @@ import java.util.Random;
 import java.util.function.Function;
 
 public class CraterFeature extends Feature<CorruptedFeatureConfig> {
-
     public static final ArrayList<BlockState> ALLOWED_STATES = new ArrayList<BlockState>();
-
     public CraterFeature(Function<Dynamic<?>, ? extends CorruptedFeatureConfig> function) {
         super(function);
         ALLOWED_STATES.add(Blocks.GRASS_BLOCK.getDefaultState());
         ALLOWED_STATES.add(Blocks.STONE.getDefaultState());
         ALLOWED_STATES.add(Blocks.GRAVEL.getDefaultState());
         ALLOWED_STATES.add(Blocks.SAND.getDefaultState());
-    }
-
-    private static boolean canSpawn(IWorld world, BlockPos pos) {
-        BlockState state = world.getBlockState(pos);
-        if (state == Blocks.GRASS_BLOCK.getDefaultState() || state == Blocks.PODZOL.getDefaultState() || state == Blocks.COARSE_DIRT.getDefaultState())
-            return true;
-        return state == Blocks.GRAVEL.getDefaultState() || state == Blocks.SAND.getDefaultState() || state == Blocks.STONE.getDefaultState();
+//        ALLOWED_STATES.add(Blocks.GRASS_BLOCK.getDefaultState());
     }
 
     @Override
@@ -45,20 +37,16 @@ public class CraterFeature extends Feature<CorruptedFeatureConfig> {
                     for (int j = -(amt + scale); j <= (amt + scale); j++) {
                         if (i == -(amt + scale) || i == (amt + scale) || j == -(amt + scale) || j == (amt + scale)) {
                             if (!Rands.chance(3)) {
-//                                this.setBlockState(world, new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.AIR.getDefaultState());
-                                world.setBlockState(new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.AIR.getDefaultState(), 11);
+                                this.setBlockState(world, new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.AIR.getDefaultState());
                             }
                             if (config.corrupted && Rands.chance(3)) {
-//                                this.setBlockState(world, new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.NETHERRACK.getDefaultState());
-                                world.setBlockState(new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.NETHERRACK.getDefaultState(), 11);
+                                this.setBlockState(world, new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.NETHERRACK.getDefaultState());
                                 if (Rands.chance(2)) {
-//                                    this.setBlockState(world, new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt) + 1, pos.getZ() + j), Blocks.FIRE.getDefaultState());
-                                    world.setBlockState(new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt) + 1, pos.getZ() + j), Blocks.AIR.getDefaultState(), 11);
+                                    this.setBlockState(world, new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt) + 1, pos.getZ() + j), Blocks.FIRE.getDefaultState());
                                 }
                             }
                         } else {
-//                            this.setBlockState(world, new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.AIR.getDefaultState());
-                            world.setBlockState(new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.AIR.getDefaultState(), 11);
+                            this.setBlockState(world, new BlockPos(pos.getX() + i, pos.getY() - (amtMax - amt), pos.getZ() + j), Blocks.AIR.getDefaultState());
                         }
                     }
                 }
@@ -66,5 +54,12 @@ public class CraterFeature extends Feature<CorruptedFeatureConfig> {
         }
 
         return true;
+    }
+
+    private static boolean canSpawn(IWorld world, BlockPos pos) {
+        BlockState state = world.getBlockState(pos);
+        if (state == Blocks.GRASS_BLOCK.getDefaultState() || state == Blocks.PODZOL.getDefaultState() || state == Blocks.COARSE_DIRT.getDefaultState()) return true;
+        if (state == Blocks.GRAVEL.getDefaultState() || state == Blocks.SAND.getDefaultState() || state == Blocks.STONE.getDefaultState()) return true;
+        return false;
     }
 }
--- src/main/java/io/github/vampirestudios/raa/registries/Features.java
+++ src/main/java/io/github/vampirestudios/raa/registries/Features.java
@@ -1,98 +1,27 @@
 package io.github.vampirestudios.raa.registries;
 
-import com.google.common.collect.ImmutableSet;
-import io.github.vampirestudios.raa.RandomlyAddingAnything;
-import io.github.vampirestudios.raa.generation.dimensions.DimensionData;
 import io.github.vampirestudios.raa.generation.feature.*;
 import io.github.vampirestudios.raa.generation.feature.config.CorruptedFeatureConfig;
-import io.github.vampirestudios.raa.utils.Utils;
-import net.minecraft.fluid.Fluids;
-import net.minecraft.structure.StructurePieceType;
+import net.fabricmc.fabric.api.registry.CommandRegistry;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.GenerationStep;
-import net.minecraft.world.gen.ProbabilityConfig;
-import net.minecraft.world.gen.carver.Carver;
-import net.minecraft.world.gen.carver.CarverConfig;
-import net.minecraft.world.gen.decorator.Decorator;
-import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
-import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
 
 import static io.github.vampirestudios.raa.RandomlyAddingAnything.MOD_ID;
 
 public class Features {
     public static NetherrackFeature CORRUPTED_NETHRRACK;
     public static CraterFeature CRATER_FEATURE;
-    public static TowerFeature TOWER;
+    public static OutpostFeature OUTPOST;
     public static CampfireFeature CAMPFIRE;
-    public static SmallSkeletalTreeFeature SMALL_SKELETON_TREE;
-    public static LargeSkeletalTreeFeature LARGE_SKELETON_TREE;
-    public static SpiderLairFeature SPIDER_LAIR;
-    public static SmallDeadwoodTreeFeature SMALL_DEADWOOD_TREE;
-    public static LargeDeadwoodTreeFeature LARGE_DEADWOOD_TREE;
+    public static TowerFeature TOWER;
 
     public static void init() {
-        CORRUPTED_NETHRRACK = register("corrupted_netherrack", new NetherrackFeature(DefaultFeatureConfig::deserialize));
-        CRATER_FEATURE = register("crater_feature", new CraterFeature(CorruptedFeatureConfig::deserialize));
-        TOWER = register("tower", new TowerFeature(DefaultFeatureConfig::deserialize));
-        CAMPFIRE = register("campfire", new CampfireFeature(DefaultFeatureConfig::deserialize));
-        SMALL_SKELETON_TREE = register("skeleton_tree_small", new SmallSkeletalTreeFeature(TreeFeatureConfig::deserialize));
-        LARGE_SKELETON_TREE = register("skeleton_tree_large", new LargeSkeletalTreeFeature(TreeFeatureConfig::deserialize));
-        SPIDER_LAIR = register("spider_lair", new SpiderLairFeature(DefaultFeatureConfig::deserialize));
-        SMALL_DEADWOOD_TREE = register("small_deadwood_tree", new SmallDeadwoodTreeFeature(TreeFeatureConfig::deserialize));
-        LARGE_DEADWOOD_TREE = register("large_deadwood_tree", new LargeDeadwoodTreeFeature(TreeFeatureConfig::deserialize));
+        CommandRegistry.INSTANCE.register(false, dispatcher -> CommandLocateRAAStructure.register(dispatcher));
+        CORRUPTED_NETHRRACK = Registry.register(Registry.FEATURE, new Identifier(MOD_ID, "corrupted_netherrack"), new NetherrackFeature(DefaultFeatureConfig::deserialize));
+        CRATER_FEATURE = Registry.register(Registry.FEATURE, new Identifier(MOD_ID, "crater_feature"), new CraterFeature(CorruptedFeatureConfig::deserialize));
+        OUTPOST = Registry.register(Registry.FEATURE, new Identifier(MOD_ID, "outpost"), new OutpostFeature(DefaultFeatureConfig::deserialize));
+        CAMPFIRE = Registry.register(Registry.FEATURE, new Identifier(MOD_ID, "campfire"), new CampfireFeature(DefaultFeatureConfig::deserialize));
+        TOWER = Registry.register(Registry.FEATURE, new Identifier(MOD_ID, "tower"), new TowerFeature(DefaultFeatureConfig::deserialize));
     }
-
-    public static void addDefaultCarvers(Biome biome, DimensionData dimensionData) {
-        if (Utils.checkBitFlag(dimensionData.getFlags(), Utils.TECTONIC)) {
-            CaveCarver caveCarver = registerCarver("cave_carver", new CaveCarver(dimensionData));
-            biome.addCarver(GenerationStep.Carver.AIR, Biome.configureCarver(caveCarver, new ProbabilityConfig(1)));
-            RavineCarver ravineCarver = registerCarver("ravine_carver", new RavineCarver(dimensionData));
-            biome.addCarver(GenerationStep.Carver.AIR, Biome.configureCarver(ravineCarver, new ProbabilityConfig(1)));
-        } else {
-            CaveCarver caveCarver = registerCarver("cave_carver", new CaveCarver(dimensionData));
-            biome.addCarver(GenerationStep.Carver.AIR, Biome.configureCarver(caveCarver, new ProbabilityConfig(0.14285715F)));
-            RavineCarver ravineCarver = registerCarver("ravine_carver", new RavineCarver(dimensionData));
-            biome.addCarver(GenerationStep.Carver.AIR, Biome.configureCarver(ravineCarver, new ProbabilityConfig(0.02F)));
-        }
-    }
-
-    public static void addDefaultSprings(Biome biome, DimensionData data) {
-        biome.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Feature.SPRING_FEATURE.configure(new SpringFeatureConfig(Fluids.WATER.getDefaultState(), true, 4, 1, ImmutableSet.of(Registry.BLOCK.get(new Identifier(RandomlyAddingAnything.MOD_ID, data.getName().toLowerCase() + "_stone"))))).createDecoratedFeature(Decorator.COUNT_BIASED_RANGE.configure(new RangeDecoratorConfig(50, 8, 8, 256))));
-        biome.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, Feature.SPRING_FEATURE.configure(new SpringFeatureConfig(Fluids.LAVA.getDefaultState(), true, 4, 1, ImmutableSet.of(Registry.BLOCK.get(new Identifier(RandomlyAddingAnything.MOD_ID, data.getName().toLowerCase() + "_stone"))))).createDecoratedFeature(Decorator.COUNT_VERY_BIASED_RANGE.configure(new RangeDecoratorConfig(20, 8, 16, 256))));
-    }
-
-    public static <C extends FeatureConfig, F extends Feature<C>> F register(String name, F feature) {
-        if (!Registry.FEATURE.containsId(new Identifier(MOD_ID, name))) {
-            return Registry.register(Registry.FEATURE, new Identifier(MOD_ID, name), feature);
-        } else {
-            return feature;
-        }
-    }
-
-    public static <F extends StructureFeature<?>> F registerStructure(String name, F structureFeature) {
-        if (!Registry.STRUCTURE_FEATURE.containsId(new Identifier(MOD_ID, name))) {
-            return Registry.register(Registry.STRUCTURE_FEATURE, new Identifier(MOD_ID, name), structureFeature);
-        } else {
-            return structureFeature;
-        }
-    }
-
-    public static <F extends StructurePieceType> F registerStructurePiece(String name, F structurePieceType) {
-        if (!Registry.STRUCTURE_PIECE.containsId(new Identifier(MOD_ID, name))) {
-            return Registry.register(Registry.STRUCTURE_PIECE, new Identifier(MOD_ID, name), structurePieceType);
-        } else {
-            return structurePieceType;
-        }
-    }
-
-    public static <F extends CarverConfig, C extends Carver<F>> C registerCarver(String name, C carver) {
-        if (!Registry.CARVER.containsId(new Identifier(MOD_ID, name))) {
-            return Registry.register(Registry.CARVER, new Identifier(MOD_ID, name), carver);
-        } else {
-            return carver;
-        }
-    }
-
 }
