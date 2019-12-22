96305e3702f97b24bac133d643aff9997e9b926a
OliviaTheVampire <sindrefagerheim53@gmail.com>
2019-12-22 17:22:57 +0100

    replaced a lot of the modImplementation's with modRuntime, updated simplex terrain, yarn, loader and api and also fixed mixin bugs and working more on adding support for The Hallow

--- src/main/java/io/github/vampirestudios/raa/registries/CustomTargets.java
+++ src/main/java/io/github/vampirestudios/raa/registries/CustomTargets.java
@@ -1,13 +1,18 @@
 package io.github.vampirestudios.raa.registries;
 
+import com.google.common.collect.ImmutableList;
 import io.github.vampirestudios.raa.utils.RegistryUtils;
 import io.github.vampirestudios.raa.world.gen.feature.OreFeatureConfig;
+import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.predicate.block.BlockPredicate;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class CustomTargets {
 
@@ -57,24 +62,36 @@ public class CustomTargets {
     public static OreFeatureConfig.Target END_STONE;
     public static OreFeatureConfig.Target DOES_NOT_APPEAR;
 
-    public static void init() {
+    private static Set<Identifier> identifiers = new HashSet<>();
 
+    public static void init() {
         END_STONE = RegistryUtils.registerOreTarget("end_stone", new BlockPredicate(Blocks.END_STONE), Blocks.END_STONE);
         DOES_NOT_APPEAR = RegistryUtils.registerOreTarget("does_not_appear", new BlockPredicate(null), null);
 
+        identifiers.addAll(
+                ImmutableList.of(
+                        new Identifier("thehallow", "deceased_grass_block"),
+                        new Identifier("thehallow", "deceased_dirt"),
+                        new Identifier("thehallow", "tainted_stone"),
+                        new Identifier("thehallow", "tainted_sand"),
+                        new Identifier("thehallow", "tainted_sandstone"),
+                        new Identifier("thehallow", "tainted_gravel")
+                )
+        );
+
         if (FabricLoader.getInstance().isModLoaded("thehallow")) {
-            Block grassBlock = Registry.BLOCK.get(new Identifier("thehallow", "deceased_grass_block"));
-            RegistryUtils.registerOreTarget(new Identifier("thehallow", "deceased_grass_block"), new BlockPredicate(grassBlock), grassBlock);
-            Block dirtBlock = Registry.BLOCK.get(new Identifier("thehallow", "deceased_dirt"));
-            RegistryUtils.registerOreTarget(new Identifier("thehallow", "deceased_dirt"), new BlockPredicate(dirtBlock), dirtBlock);
-            Block stoneBlock = Registry.BLOCK.get(new Identifier("thehallow", "tainted_stone"));
-            RegistryUtils.registerOreTarget(new Identifier("thehallow", "tainted_stone"), new BlockPredicate(stoneBlock), stoneBlock);
-            Block taintedSand = Registry.BLOCK.get(new Identifier("thehallow", "tainted_sand"));
-            RegistryUtils.registerOreTarget(new Identifier("thehallow", "tainted_sand"), new BlockPredicate(taintedSand), taintedSand);
-            Block taintedSandstone = Registry.BLOCK.get(new Identifier("thehallow", "tainted_sandstone"));
-            RegistryUtils.registerOreTarget(new Identifier("thehallow", "tainted_sandstone"), new BlockPredicate(taintedSandstone), taintedSandstone);
-            Block taintedGravel = Registry.BLOCK.get(new Identifier("thehallow", "tainted_gravel"));
-            RegistryUtils.registerOreTarget(new Identifier("thehallow", "tainted_gravel"), new BlockPredicate(taintedGravel), taintedGravel);
+            if (Registry.BLOCK.containsId(new Identifier("thehallow", "tainted_stone"))) {
+                for (Identifier identifier : identifiers) {
+                    Block block = Registry.BLOCK.get(identifier);
+                    RegistryUtils.registerOreTarget(identifier, new BlockPredicate(block), block);
+                }
+            } else {
+                RegistryEntryAddedCallback.event(Registry.BLOCK).register(((i, identifier, block) -> {
+                    if (identifiers.contains(identifier)) {
+                        RegistryUtils.registerOreTarget(identifier, new BlockPredicate(block), block);
+                    }
+                }));
+            }
         }
     }
 
