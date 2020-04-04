48b197a54b097e3e092036f70cfbbdc5ba5bd902
oliviathevampire <sindrefagerheim53@gmail.com>
2020-04-04 14:29:31 +0200

    updated dependencies, rebalanced materials and dimension materials, cleaned up in some code and also removed some console spam

--- src/main/java/io/github/vampirestudios/raa/blocks/DimensionalStone.java
+++ src/main/java/io/github/vampirestudios/raa/blocks/DimensionalStone.java
@@ -1,6 +1,8 @@
 package io.github.vampirestudios.raa.blocks;
 
+import io.github.vampirestudios.raa.generation.dimensions.data.DimensionData;
 import io.github.vampirestudios.raa.utils.Rands;
+import io.github.vampirestudios.vampirelib.utils.Utils;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -15,18 +17,17 @@ import java.util.List;
 import static io.github.vampirestudios.raa.RandomlyAddingAnything.MOD_ID;
 
 public class DimensionalStone extends Block {
-    private String dimensionName;
+    private final DimensionData dimensionData;
 
-    public DimensionalStone(String dimensionName) {
-        super(Settings.copy(Blocks.STONE).strength(Rands.randFloatRange(0.25f, 4), Rands.randFloatRange(4, 20))
-                .jumpVelocityMultiplier(Rands.randFloatRange(1.0F, 3.0F)));
-        this.dimensionName = dimensionName;
+    public DimensionalStone(DimensionData dimensionData) {
+        super(Settings.copy(Blocks.STONE).strength(dimensionData.getStoneHardness(), dimensionData.getStoneResistance()));
+        this.dimensionData = dimensionData;
     }
 
     @Override
     public List<ItemStack> getDroppedStacks(BlockState state, LootContext.Builder builder) {
         List<ItemStack> list = new ArrayList<>();
-        list.add(new ItemStack(Registry.BLOCK.get(new Identifier(MOD_ID, dimensionName.toLowerCase() + "_cobblestone")).asItem()));
+        list.add(new ItemStack(Registry.BLOCK.get(Utils.appendToPath(dimensionData.getId(), "_cobblestone")).asItem()));
         return list;
     }
 }
